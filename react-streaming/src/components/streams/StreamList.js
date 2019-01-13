a6e4b67a3124a9610fe5d587593d34a30c99b9fd
johnmorris4 <johnmorris@morrisje.com>
2019-01-13 06:27:02 -0500

    Finished the Shop page.  setting up the RMTP server

--- react-streaming/src/components/streams/StreamList.js
+++ react-streaming/src/components/streams/StreamList.js
@@ -24,7 +24,7 @@ class StreamList extends Component {
                     <div className="item" key={stream.id}>
                     {this.renderAdmin(stream)}
                         <i className="large middle aligned icon camera" />
-                        <div className="content">{stream.title} 
+                        <div className="content"><Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link> 
                         <div className="description">{stream.description}</div>
                         </div>
                         
